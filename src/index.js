21fd42d692e21059b8b4f058d52b0a331f7706eb
IgorVeremeyenko <viruscomp7@gmail.com>
2022-06-27 21:33:42 +0200

    init: add code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    return n < 0 ? parseFloat(n.toString().split('').reverse().join('')) * Math.sign(n * -1) 
+    :
+     parseFloat(n.toString().split('').reverse().join('')) * Math.sign(n);
 }
